bc58960ef8622b78a2b3c7191c7c3e0e73b617d5
KH8 <reczek.krzysztof@gmail.com>
2014-08-27 15:28:06 +0200

    First

---
+++ PlcAgentLicenceGenerator/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PlcAgentLicenceGenerator.Signature;
+
+namespace PlcAgentLicenceGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SignatureLabel.Content = "PC Signature: \n" + SignatureHandler.GetSignature();
+        }
+    }
+}
