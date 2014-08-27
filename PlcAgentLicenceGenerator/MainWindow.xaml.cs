57b6e32126eb86ced82aff171237cacb36531c91
KH8 <reczek.krzysztof@gmail.com>
2014-08-27 15:50:56 +0200

    BlowFish implemented

--- PlcAgentLicenceGenerator/MainWindow.xaml.cs
+++ PlcAgentLicenceGenerator/MainWindow.xaml.cs
@@ -11,7 +11,16 @@ namespace PlcAgentLicenceGenerator
         {
             InitializeComponent();
 
-            SignatureLabel.Content = "PC Signature: \n" + SignatureHandler.GetSignature();
+            var signature = SignatureHandler.GetSignature();
+            SignatureLabel.Content = "PC Signature: \n" + signature;
+
+            var blowFishEncryptor = new BlowFish.BlowFish(HexKey.Value);
+
+            var encryptedSignature = blowFishEncryptor.Encrypt_CTR(signature);
+            SignatureEncryptedLabel.Content = "PC Signature Encrypted: \n" + encryptedSignature;
+
+            var decryptedSignature = blowFishEncryptor.Decrypt_CTR(encryptedSignature);
+            SignatureDecryptedLabel.Content = "PC Signature Decrypted: \n" + decryptedSignature;
         }
     }
 }
